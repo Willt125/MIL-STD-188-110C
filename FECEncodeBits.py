36c6f336b478f9da81b711f9438606ee2c9fb674
AbNormal <abnormal@youjo.love>
2023-01-10 15:32:39 +0000

    Update 'FECEncodeBits.py'
    Signed-off-by: AbNormal <abnormal@youjo.love>

--- FECEncodeBits.py
+++ FECEncodeBits.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# The repeat factors define how many times to repeat the output of the FEC buffer into the output bitstream.
+# Defined in MIL-STD-188-110, page 26 & 28 (32 & 24 in the PDF)
 fixed_repeat_factor = {
         75: 2,
         150: 8,
@@ -29,36 +31,34 @@ def FECEncodeBits(inArray: np.array[int], Bd: int, fqmode: str ='fixed') -> np.a
 	   the odd bits are encoded using x^6+x^5+x^4+x^3+1.
 	   These bits are repeat coded depending on the selected mode."""
 
-        fec_buffer = [0, 0, 0, 0, 0, 0, 1] # Initialize the FEC buffer
+        fec_buffer = [0, 0, 0, 0, 0, 0, 1] # Initialize the FEC buffer. The seventh position is a static '1'
         x = 0 # initialize FEC output pointer
 
         if fqmode == 'fixed':
                 if Bd != 4800:
-                        fec_out = [None]*(len(inArray)*fixed_repeat_factor[Bd])
+                        fec_out = [None]*(len(inArray)*fixed_repeat_factor[Bd]) # Prepare the output bitstream.
                 else:
                         return inArray # 4800-baud does not receive error correction
 
                 for bit in inArray:
-                        fec_buffer[0] = bit
+                        fec_buffer[0] = bit # load the lowest bit position with the input bit
                         bit1 = fec_buffer[0] ^ fec_buffer[2] ^ fec_buffer[3] ^ fec_buffer[5] ^ fec_buffer[6] # The error correction is just a simple convolution.
                         bit2 = fec_buffer[0] ^ fec_buffer[1] ^ fec_buffer[2] ^ fec_buffer[3] ^ fec_buffer[6] # There is probably a more efficient way to do this.
                                         
-                        fec_out[x:x+fixed_repeat_factor[Bd]] = [bit1, bit2]*fixed_repeat_factor[Bd]
-                        x += fixed_repeat_factor[Bd]
+                        fec_out[x:x+fixed_repeat_factor[Bd]] = [bit1, bit2]*fixed_repeat_factor[Bd] # load the output array with the current batch
+                        x += fixed_repeat_factor[Bd] # and shift the output pointer
                         
-                        fec_buffer[5] = fec_buffer[4]
-                        fec_buffer[4] = fec_buffer[3]
-                        fec_buffer[3] = fec_buffer[2]
-                        fec_buffer[2] = fec_buffer[1]
-                        fec_buffer[1] = fec_buffer[0]
+
+                        fec_buffer[0:6] = [0] + fec_buffer[0:5] # now shift the rest of the fec_buffer to the right, leaving the static '1' in place
+                        
                 fec_out = np.array(fec_out, dtype=int).flatten()
                 return fec_out
 
         elif fqmode == 'hopping':
                 if Bd != 4800:
-                        fec_out = [None]*(len(inArray)*hopping_repeat_factor[Bd])
+                        fec_out = [None]*(len(inArray)*hopping_repeat_factor[Bd]) # prepare the output bitstream
                 else:
-                        return inArray
+                        return inArray # 4800-baud does not get FEC correction
 
                 for bit in inArray:
                         fec_buffer[0] = bit
@@ -69,13 +69,10 @@ def FECEncodeBits(inArray: np.array[int], Bd: int, fqmode: str ='fixed') -> np.a
                                 fec_out[x:x+hopping_repeat_factor[Bd]] = [bit1, bit2]*hopping_repeat_factor[Bd]
                                 x += hopping_repeat_factor[Bd]
                         else:
-                                fec_out[x:x+3] = [bit1, bit2, bit1]
+                                fec_out[x:x+3] = [bit1, bit2, bit1] # 2400-baud hopping mode needs a special case due to bit puncturing
                                 x += 3
                         
-                        fec_buffer[5] = fec_buffer[4]
-                        fec_buffer[4] = fec_buffer[3]
-                        fec_buffer[3] = fec_buffer[2]
-                        fec_buffer[2] = fec_buffer[1]
-                        fec_buffer[1] = fec_buffer[0]
-                fec_out = np.array(fec_out, dtype=int).flatten()
+                        fec_buffer[0:6] = [0] + fec_buffer[0:5] # now shift the rest of the fec_buffer to the right, leaving the static '1' in place
+
+                fec_out = np.array(fec_out, dtype=int).flatten() # ensure the output array is flat
                 return fec_out
