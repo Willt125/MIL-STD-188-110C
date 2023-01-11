f33ae38670069658877a64b5ce29badc0c6ebed9
AbNormal <abnormal@youjo.love>
2023-01-11 19:29:22 +0000

    Update 'MGDDecode.py'

--- MGDDecode.py
+++ MGDDecode.py
@@ -1,6 +1,24 @@
 import numpy as np
 
-def MGD_Decode(indata, Bd, frqmode):
+tribit_mgd_map = {
+    0: 0,
+    1: 1,
+    2: 3,
+    3: 2,
+    4: 7,
+    5: 6,
+    6: 4,
+    7: 5
+}
+
+dibit_mgd_map = {
+    0: 0,
+    1: 1,
+    2: 3,
+    3: 2
+}
+
+def MGD_Decode(indata: np.array[int], Bd: int, frqmode: str) -> np.array[int]:
     # Convert the input bitgroups to their Gray form, so that
     # receive errors only result in one bit in err.
     
@@ -9,47 +27,20 @@ def MGD_Decode(indata, Bd, frqmode):
         for i in range(0,len(indata),2):
             grouped[i//2] = int(str(indata[i]) + str(indata[i+1]), base=2)
         out = np.zeros(grouped.shape,dtype=int)
-        for i, el in enumerate(grouped):
-            if el == 0:
-                out[i] = el
-            elif el == 1:
-                out[i] = el
-            elif el == 2:
-                out[i] = 3
-            elif el == 3:
-                out[i] = 2
-            else:
-                raise RuntimeError("Invalid symbol in bitstream.")
+        for el in grouped:
+            out[i] = dibit_mgd_map[el]
         return out
+    
     elif (Bd == 150 or Bd == 300 or Bd == 600 or (Bd == 75 and frqmode == 'hopping')):
-        out = np.zeros(indata.shape,dtype=int)
-        for i in range(len(indata)):
-            out[i] = int(str(indata[i]),base=2)
-        return out
+        return indata
+    
     elif (Bd == 2400 or Bd == 4800):
         grouped = np.zeros([indata.shape[0]//3,],dtype=int)
-        for i in range(0,len(indata)-1,3):
+        for i in range(0,len(indata),3):
             grouped[i//3] = int(str(indata[i]) + str(indata[i+1]) + str(indata[i+2]), base=2)
         out = np.zeros(grouped.shape,dtype=int)
-        for i, el in enumerate(grouped):
-            if el == 0:
-                out[i] = el
-            elif el == 1:
-                out[i] = el
-            elif el == 2:
-                out[i] = 3
-            elif el == 3:
-                out[i] = 2
-            elif el == 4:
-                out[i] = 7
-            elif el == 5:
-                out[i] = 6
-            elif el == 6:
-                out[i] = 4
-            elif el == 7:
-                out[i] = 5
-            else:
-                raise RuntimeError("Invalid symbol in bitstream.")
+        for el in grouped:
+            out[i] = tribit_mgd_map[el]
         return out
     else:
         raise RuntimeError("Invalid input.")
