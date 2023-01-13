d68ac79e8aab49c587ab7e9f580742dfa35fa67f
AbNormal <abnormal@youjo.love>
2023-01-13 16:15:00 +0000

    Update 'ChannelMap.py'
    Implement known data probes correctly(?)

--- ChannelMap.py
+++ ChannelMap.py
@@ -1,164 +1,123 @@
 import numpy as np
 
-def channelMap(indata, Bd, inter_len, frqmode, data_type):
-  # Map the input symbols to their final forms, adding known data probes (commented below)
-  # and repeat symbols where necessary. We're almost done, woohoo!
-  # This is where a lot of the heavy lifting is done, and using numpy arrays gets REALLY slow.
-  # Unfortunately I think the only way to get faster is to move to C/C++, which I'm already doing.
-  
-    if frqmode == 'fixed':
-        if Bd == 75:
-            if inter_len == "Z" or inter_len == "S":
-                lenMod = 45
-            elif inter_len == "L":
-                lenMod = 360
-            map_out = []
-            for i in range(len(indata)):
-                if ((i % lenMod == 0) and (i != 0)):
-                    if indata[i] == 0:
-                        map_out += [0,0,0,0] * 8
-                    elif indata[i] == 1:
-                        map_out += [0,4,0,4] * 8
-                    elif indata[i] == 2:
-                        map_out += [0,0,4,4] * 8
-                    elif indata[i] == 3:
-                        map_out += [0,4,4,0] * 8
-                    else:
-                        raise RuntimeError("Invalid symbol in stream.")
-                else:
-                    if indata[i] == 0:
-                        map_out += [0,0,0,0,4,4,4,4] * 4
-                    elif indata[i] == 1:
-                        map_out += [0,4,0,4,4,0,4,0] * 4
-                    elif indata[i] == 2:
-                        map_out += [0,0,4,4,4,4,0,0] * 4
-                    elif indata[i] == 3:
-                        map_out += [0,4,4,0,4,0,0,4] * 4
-                    else:
-                        raise RuntimeError("Invalid symbol in stream.")
-            map_out = np.array(map_out)
-            return map_out
-        elif Bd == 150 or Bd == 300 or Bd == 600:
-            unknownMod = 20
-            map_out = []
-            i = 0
-            affecting = False
-            while (i < len(indata)):
-                if affecting == False:
-                    for j in range(i,i + unknownMod):
-                        if indata[j] == 0:
-                            map_out += [0]
-                        elif indata[j] == 1:
-                            map_out += [4]
-                        else:
-                            raise RuntimeError("Invalid symbol in stream.")
-                    i += unknownMod
-                    affecting = True
-                else:
-                    if Bd == 150:
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,4,4,0,4,0,0,4] * 2
-                            D2 = [0,0,0,0,4,4,4,4] * 2
-                            preblock = [0] * 1420
-                        elif inter_len == "L":
-                            D1 = [0,4,0,4,4,0,4,0] * 2
-                            D2 = [0,0,0,0,4,4,4,4] * 2
-                            preblock = [0] * 11500
-                    elif Bd == 300:
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,0,4,4,4,4,0,0] * 2
-                            D2 = [0,4,4,0,4,0,0,4] * 2
-                            preblock = [0] * 1420
-                        elif inter_len == "L":
-                            D1 = [0,0,0,0,4,4,4,4] * 2
-                            D2 = [0,4,4,0,4,0,0,4] * 2
-                            preblock = [0] * 11500
-                    elif Bd == 600:
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,0,4,4,4,4,0,0] * 2
-                            D2 = [0,0,4,4,4,4,0,0] * 2
-                            preblock = [0] * 1420
-                        elif inter_len == "L":
-                            D1 = [0,0,0,0,4,4,4,4] * 2
-                            D2 = [0,0,4,4,4,4,0,0] * 2
-                            preblock = [0] * 11500
-                    map_out = map_out + D1 + D2 + [0, 0, 0, 0]
-                    affecting = False
-            map_out = np.array(map_out)
-            return map_out
-        elif Bd == 1200:
-            unknownMod = 20
-            knownMod = 20
-            map_out = []
-            i = 0
-            affecting = False
-            while (i < len(indata)):
-                if affecting == False:
-                    for j in range(i,i + unknownMod):
-                        if indata[j] == 0:
-                            map_out += [0]
-                        elif indata[j] == 1:
-                            map_out += [2]
-                        elif indata[j] == 2:
-                            map_out += [4]
-                        elif indata[j] == 3:
-                            map_out += [6]
-                        else:
-                            raise RuntimeError("Invalid symbol in stream.")
-                    i += unknownMod
-                    affecting = True
-                else:
-                    if inter_len == "Z" or inter_len == "S":
-                        D1 = [0,0,4,4,4,4,0,0] * 2
-                        D2 = [0,4,0,4,4,0,4,0] * 2
-                        preblock = [0] * 1420
-                    elif inter_len == "L":
-                        D1 = [0,0,0,0,4,4,4,4] * 2
-                        D2 = [0,4,0,4,4,0,4,0] * 2
-                        preblock =  [0] * 11500
-                    map_out = map_out + D1 + D2 + [0, 0, 0, 0]
-                    affecting = False
-            map_out = np.array(map_out)
-            return map_out
-        elif Bd == 2400 or Bd == 4800:
-            unknownMod = 32
-            map_out = []
-            i = 0
-            affecting = False
-            while (i < len(indata)):
-                if affecting == False:
-                    for j in range(i,i + unknownMod):
-                        try:
-                            map_out += [indata[j]]
-                        except:
-                            pass
-                    i += unknownMod
-                    affecting = True
-                else:
-                    if Bd == 2400 and data_type == "Data":
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,0,4,4,4,4,0,0] * 2
-                            D2 = [0,0,0,0,4,4,4,4] * 2
-                            preblock = [0] * 1424
-                        elif inter_len == "L":
-                            D1 = [0,0,0,0,4,4,4,4] * 2
-                            D2 = [0,0,0,0,4,4,4,4] * 2
-                            preblock = [0] * 11504
-                    elif Bd == 2400 and data_type == "Voice":
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,4,4,0,4,0,0,4] * 2
-                            D2 = [0,4,4,0,4,0,0,4] * 2
-                            preblock = [0] * 1424
-                        elif inter_len == "L":
-                            raise RuntimeError("Invalid input.")
-                    elif Bd == 4800:
-                        if inter_len == "Z" or inter_len == "S":
-                            D1 = [0,4,4,0,4,0,0,4] * 2
-                            D2 = [0,0,4,4,4,4,0,0] * 2
-                            preblock = [0] * 1424
-                        elif inter_len == "L":
-                            raise RuntimeError("Invalid input.")
-                    map_out = map_out + D1 + D2
-                    affecting = False
-            map_out = np.array(map_out)
-            return map_out
+D1D2_conv = {
+    (150, "Z", _): [0, 4, 0, 4, 4, 0, 4, 0, 0, 0, 0, 0, 4, 4, 4, 4],
+    (150, "S", _): [0, 4, 0, 4, 4, 0, 4, 0, 0, 0, 0, 0, 4, 4, 4, 4],
+    (150, "L", _): [0, 4, 4, 0, 4, 0, 0, 4, 0, 0, 0, 0, 4, 4, 4, 4],
+    (300, "Z", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 4, 4, 0, 4, 0, 0, 4],
+    (300, "S", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 4, 4, 0, 4, 0, 0, 4],
+    (300, "L", _): [0, 0, 4, 4, 4, 4, 0, 0, 0, 4, 4, 0, 4, 0, 0, 4],
+    (600, "Z", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 4, 4, 4, 4, 0, 0],
+    (600, "S", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 4, 4, 4, 4, 0, 0],
+    (600, "L", _): [0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0],
+    (1200, "Z", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 4, 0, 4, 4, 0, 4, 0],
+    (1200, "S", _): [0, 0, 0, 0, 4, 4, 4, 4, 0, 4, 0, 4, 4, 0, 4, 0],
+    (1200, "L", _): [0, 0, 4, 4, 4, 4, 0, 0, 0, 4, 0, 4, 4, 0, 4, 0],
+    (2400, "Z", "Data"): [0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4],
+    (2400, "S", "Data"): [0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4],
+    (2400, "L", "Data"): [0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 4, 4, 4, 4],
+    (2400, "Z", "Voice"): [0, 4, 4, 0, 4, 0, 0, 4, 0, 4, 4, 0, 4, 0, 0, 4],
+    (2400, "S", "Voice"): [0, 4, 4, 0, 4, 0, 0, 4, 0, 4, 4, 0, 4, 0, 0, 4],
+    (2400, "L", "Voice"): [None],
+    (4800, "Z", _): [0, 4, 4, 0, 4, 0, 0, 4, 0, 0, 4, 4, 4, 4, 0, 0],
+    (4800, "S", _): [0, 4, 4, 0, 4, 0, 0, 4, 0, 0, 4, 4, 4, 4, 0, 0],
+    (4800, "L", _): [None]
+}
+
+75bps_fixed_constellation_map_normal = {
+    0: [0, 0, 0, 0] * 8,
+    1: [0, 4, 0, 4] * 8,
+    2: [0, 0, 4, 4] * 8,
+    3: [0, 4, 4, 0] * 8 
+}
+
+75bps_fixed_constellation_map_exceptional = {
+    0: [0, 0, 0, 0, 4, 4, 4, 4] * 4,
+    1: [0, 4, 0, 4, 4, 0, 4, 0] * 4,
+    2: [0, 0, 4, 4, 4, 4, 0, 0] * 4,
+    3: [0, 4, 4, 0, 4, 0, 0, 4] * 4 
+}
+
+150_600bps_constellation_map = {
+    0: 0,
+    1: 4
+}
+
+
+1200bps_constellation_map = {
+    0: 0,
+    1: 2,
+    2: 4,
+    3: 6 
+}
+
+def channelMapChunk(indata: np.array[int], Bd: int, interleave_length, freq_mode):
+  """Map the input symbols to their final forms. We're almost done, woohoo!
+     `indata` in the input array from the MGD
+     `Bd` is the baud rate
+     `interleave_length` is the interleaver block size, either Z, S, or L
+     `freq_mode` is the current frequency mode, either fixed or hopping"""
+
+    if freq_mode == 'fixed' and Bd == 75:
+        len_mod = 45 if interleave_length == "S" or interleave_length == "Z" else 360
+        map_out = []
+        for i, symbol in enumerate(indata):
+            if (i % len_mod == 0) and (i != 0):
+                map_out += 75bps_fixed_constellation_map_exceptional[symbol]
+            else:
+                map_out += 75bps_fixed_constellation_map_normal[symbol]
+        return np.array(map_out, dtype=int)
+    
+    if Bd == 75:
+        map_out = []
+        for symbol in indata:
+            map_out += 150_600bps_constellation_map[symbol]
+        return np.array(map_out, dtype=int)
+    
+    if (Bd == 150 | Bd == 300 | Bd == 600):
+        map_out = []
+        for symbol in indata:
+            map_out += 150_600bps_constellation_map[symbol]
+        return np.array(map_out, dtype=int)
+        
+    if Bd == 1200:
+        map_out = []
+        for symbol in indata:
+            map_out += 1200bps_constellation_map[symbol]
+        return np.array(map_out, dtype=int)
+
+    return indata
+        
+def channelMap(indata: np.array[int], Bd: int, interleave_length, freq_mode):
+    if Bd == 75 and freq_mode == "fixed":
+        return channelMapChunk(indata, Bd, interleave_length, freq_mode)
+    
+    probe_len = 16 if Bd in (2400, 4800) else 20
+    len_mod = 32 if Bd in (2400, 4800) else 20
+
+    if Bd in (150, 300, 600, 1200):
+        map_out = np.array([], dtype=int)
+        for i in range(0, len(indata), len_mod):
+            map_out = np.concatenate((map_out, channelMapChunk(indata[i:i+len_mod], Bd, interleave_length, freq_mode)))
+            probe = [0] * probe_len
+            probe[0:] = D1D2_conv[Bd, interleave_len, frqmode]
+            map_out = np.concatenate((map_out, probe))
+        return map_out
+    
+    if Bd == 2400:
+        map_out = np.array([], dtype=int)
+        for i in range(0, len(indata), len_mod):
+            map_out = np.concatenate((map_out, channelMapChunk(indata[i:i+len_mod], Bd, interleave_length, freq_mode)))
+            probe = [0] * probe_len
+            probe[0:] = D1D2_conv[Bd, interleave_len, frqmode]
+            map_out = np.concatenate((map_out, probe))
+        return map_out
+    
+    if Bd == 4800:
+        map_out = np.array([], dtype=int)
+        for i in range(0, len(indata), len_mod):
+            map_out = np.concatenate((map_out, channelMapChunk(indata[i:i+len_mod], Bd, interleave_length, freq_mode)))
+            probe = [0] * probe_len
+            probe[0:] = D1D2_conv[Bd, "S" if interleave_len == "L" else interleave_len, frqmode]
+            map_out = np.concatenate((map_out, probe))
+        return map_out
